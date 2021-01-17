b23341ba73d201600cd37dca22a2a1cee0d29f37
amanksingh007 <amanksingh007@gmail.com>
2021-01-18 01:30:48 +0530

    Sample-Server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express= require("express")
+const app=express();
+
+app.get("/health",(req,res)=>{
+    return res.status(200).send({res:"Server Healthy"});
+});
+
+const PORT = 3000;
+app.listen(PORT,()=>{
+    console.log("App s Running on ${PORT}");
+})
